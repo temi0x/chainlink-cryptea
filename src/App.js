24f1a2d008f612a09b251c54b404544564ca059e
george-hub331 <idiaghegeorge9@gmail.com>
2022-05-14 23:45:10 +0100

    [feat]: add loader

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import About from "./pages/about";
 import Home from "./pages/Home";
+import {useEffect, useState} from "react";
 import Signup from "./pages/auth/signup";
 import Dash from './pages/dashboard';
+import Loader from './components/loader';
 import Notfound from "./pages/error/404";
 import LoginForm from "./pages/auth/login";
 import { useMoralis } from "react-moralis";
 const App = () => {
-    const { isAuthenticated } = useMoralis();
+  const { isAuthenticated, isInitializing } = useMoralis();
+  const [loading, uloading] = useState(true);
+    useEffect(() => {
+        if (isInitializing) {
+          uloading(false)
+        }
+    }, [isInitializing])
 
   return (
-      <BrowserRouter>
+     <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/dashboard" element={!isAuthenticated ? <Dash /> : <Navigate to="/"/>}/>
-          <Route path="*" element={<Notfound /> }/>
+          <Route path="/" element={loading ? <Loader /> : <Home />} />
+          <Route path="/about" element={loading ? <Loader /> : <About />} />
+          <Route path="/signup" element={loading ? <Loader /> : <Signup />} />
+          <Route path="/login" element={loading ? <Loader /> : <LoginForm />} />
+          <Route path="/dashboard" element={loading ? <Loader /> : (isAuthenticated ? <Dash /> : <Navigate to="/"/>)}/>
+          <Route path="*" element={loading ? <Loader /> : <Notfound /> }/>
         </Routes>
       </BrowserRouter>
   );
---
+++ src/components/loader/index.jsx
@@ -0,0 +1,18 @@
+import { Box, CircularProgress } from '@mui/material';
+import logo1 from "../../assets/img/cryptea.png";
+import logo from "../../assets/img/cryptea1.PNG";
+const Loader = () => {
+    return (
+      <div className="h-screen flex flex-col justify-center items-center w-full">
+        <div className="flex items-center justify-center">
+          <img src={logo} alt="cryptea" width={35} className="mr-[5px]" />
+          <img src={logo1} alt="cryptea" width={100} />
+        </div>
+        <Box className="text-[#F57059] mt-11 justify-center " sx={{ display: "flex" }}>
+          <CircularProgress className="!w-[60px] !h-[60px]" color="inherit" />
+        </Box>
+      </div>
+    );
+}
+ 
+export default Loader;
