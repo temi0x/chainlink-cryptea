842b46d94d9ec1ee43bd3b6ace92156e99fbb2ac
george-hub331 <idiaghegeorge9@gmail.com>
2022-05-15 06:23:39 +0100

    [feat]: add sidebar toggle

--- src/pages/dashboard/home.jsx
+++ src/pages/dashboard/home.jsx
@@ -1,10 +1,13 @@
 import { AreaChart, Area, Tooltip, ResponsiveContainer } from "recharts";
 import { useState } from "react";
 import { FaWallet } from "react-icons/fa";
+import { IoMdClose } from "react-icons/io";
 import { BsBoxArrowInDownLeft, BsArrowRight } from "react-icons/bs";
 import {
   TextField,
   Button,
+  Modal,
+  Box,
   TableContainer,
   TableHead,
   TableRow,
@@ -112,9 +115,83 @@ const DashHome = () => {
     setPage(0);
   };
 
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    minWidth: 300,
+    width: '50%',
+    maxWidth: 600,
+    p: 4,
+  };
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+
   return (
-    <div className="dashbody h-[calc(100%-75px)] flex px-5 pb-5">
-      <div className="mr-[20px] h-full pb-1 pt-5 pr-2 w-full cusscroller overflow-y-scroll">
+    <div className="dashbody h-[calc(100%-75px)] 2sm:pr-1 flex px-5 pb-5">
+      <div>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <div className="px-4 pt-3 pb-5 bg-white">
+              <h2 className="text-[18px] font-bold text-bold pb-[10px]">
+                Quick Transfer
+              </h2>
+              <div className="py-2">
+                <TextField
+                  label={"Amount"}
+                  fullWidth
+                  className="amount"
+                  id="Amount"
+                />
+              </div>
+              <div className="py-2">
+                <TextField
+                  label={"Account/Address"}
+                  fullWidth
+                  className="account"
+                  id="account"
+                />
+              </div>
+              <div className="py-2 flex justify-center">
+                <Button
+                  variant="contained"
+                  className="!bg-[#F57059] !mr-2 !py-[13px] !font-medium !capitalize"
+                  style={{
+                    fontFamily: "inherit",
+                  }}
+                  fullWidth
+                >
+                  Transfer{" "}
+                  <BsArrowRight className="ml-3 font-medium" size={18} />
+                </Button>
+
+                <Button
+                  onClick={handleClose}
+                  variant="contained"
+                  className="!bg-[#F57059] max-w-[100px] !ml-2 !py-[13px] !font-medium !capitalize"
+                  style={{
+                    fontFamily: "inherit",
+                  }}
+                  fullWidth
+                >
+                  Close
+                  <IoMdClose className="ml-3 font-medium" size={18} />
+                </Button>
+              </div>
+            </div>
+          </Box>
+        </Modal>
+      </div>
+
+      <div className="mr-[20px] 2sm:mr-0 h-full pb-1 pt-5 pr-2 w-full cusscroller overflow-y-scroll">
         <svg
           style={{
             width: 0,
@@ -147,44 +224,59 @@ const DashHome = () => {
           </linearGradient>
         </svg>
         <div className="w-full flex overflow-y-hidden overflow-x-scroll pb-[11px] cusscroller">
-          <div className="w-[227px] mr-7 p-3 bg-[#F57059] rounded-[4px] flex justify-between flex-col h-[138px]">
-            <div className="flex justify-between items-center">
-              <div className="flex items-center">
-                <div className="bg-white w-[32px] mr-3 h-[32px] rounded-[50%] flex items-center justify-center">
-                  <FaWallet className="text-[#F57059]" size={15} />
+          <div className="flex flex-col mr-7 justify-center">
+            <div className="w-[227px] relative p-3 bg-[#F57059] rounded-[4px] flex justify-between flex-col h-[138px]">
+              <div className="flex justify-between items-center">
+                <div className="flex items-center">
+                  <div className="bg-white w-[32px] mr-3 h-[32px] rounded-[50%] flex items-center justify-center">
+                    <FaWallet className="text-[#F57059]" size={15} />
+                  </div>
+                  <span className="text-white">Balance</span>
                 </div>
-                <span className="text-white">Balance</span>
+
+                <div className="w-[40px] h-[40px]">
+                  <ResponsiveContainer height="100%" width="100%">
+                    <AreaChart width={400} height={400} data={balance}>
+                      <Area
+                        type="monotone"
+                        dataKey="amt"
+                        strokeWidth={3}
+                        fill={`url(#${
+                          change > 0 ? "green" : "red"
+                        }) transparent`}
+                        stroke={change > 0 ? "#53D258" : "#7c2424"}
+                      />
+                      <Tooltip />
+                    </AreaChart>
+                  </ResponsiveContainer>
+                </div>
               </div>
 
-              <div className="w-[40px] h-[40px]">
-                <ResponsiveContainer height="100%" width="100%">
-                  <AreaChart width={400} height={400} data={balance}>
-                    <Area
-                      type="monotone"
-                      dataKey="amt"
-                      strokeWidth={3}
-                      fill={`url(#${change > 0 ? "green" : "red"}) transparent`}
-                      stroke={change > 0 ? "#53D258" : "#7c2424"}
-                    />
-                    <Tooltip />
-                  </AreaChart>
-                </ResponsiveContainer>
+              <div className="w-full">
+                <span className="block text-white font-bold text-[15px]">
+                  ${balance.pop().amt}
+                </span>
+                <span
+                  style={{
+                    color: change > 0 ? "#53D258" : "#7c2424",
+                  }}
+                  className={`block text-[13px]`}
+                >
+                  {change > 0 ? "+" + change.toFixed(2) : change.toFixed(2)}
+                </span>
               </div>
             </div>
-
-            <div className="w-full">
-              <span className="block text-white font-bold text-[15px]">
-                ${balance.pop().amt}
-              </span>
-              <span
-                style={{
-                  color: change > 0 ? "#53D258" : "#7c2424",
-                }}
-                className={`block text-[13px]`}
-              >
-                {change > 0 ? "+" + change.toFixed(2) : change.toFixed(2)}
-              </span>
-            </div>
+            <Button
+              variant="contained"
+              className="!bg-[#F57059] !2sm:block !hidden mt-4 !py-[8px] absolute !font-medium !capitalize"
+              style={{
+                fontFamily: "inherit",
+              }}
+              fullWidth
+              onClick={handleOpen}
+            >
+              Transfer
+            </Button>
           </div>
 
           <div className="w-[227px] p-3 bg-transparent border-solid border-[1px] rounded-[4px] flex justify-between flex-col h-[138px]">
@@ -230,7 +322,7 @@ const DashHome = () => {
           </div>
         </div>
 
-        <div className="bg-white p-3 border-solid border-[1px] border-[#e3e3e3]">
+        <div className="bg-white mt-6 p-3 border-solid border-[1px] border-[#e3e3e3]">
           <div className="py-[10px]">
             <h2 className="text-[16px] font-bold text-bold">Portfolio</h2>
           </div>
@@ -243,7 +335,7 @@ const DashHome = () => {
                       key={column.id}
                       align={column.align}
                       style={{ minWidth: column.minWidth }}
-                      className="border-b-solid"
+                      className="border-b-solid !text-[#A9A9A9]"
                     >
                       {column.label}
                     </TableCell>
@@ -288,7 +380,7 @@ const DashHome = () => {
         </div>
       </div>
 
-      <div className="min-w-[337px] pt-5 h-full">
+      <div className="min-w-[337px] 2sm:hidden pt-5 h-full">
         <div className="px-4 pt-3 pb-5 mb-10 bg-white border-[1px] border-solid border-[#E3E3E3] rounded-[4px]">
           <h2 className="text-[18px] font-bold text-bold pb-[10px]">
             Quick Transfer
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -3,9 +3,10 @@ import {
   BiPaperPlane,
   BiCreditCard,
   BiLogOut,
-  BiSearch,
+  // BiSearch,
   BiBell,
 } from "react-icons/bi";
+import { IoIosArrowDropleftCircle, IoIosArrowDroprightCircle } from 'react-icons/io'; 
 import { RiSettingsLine } from "react-icons/ri";
 import logo1 from "../../assets/img/cryptea.png";
 import logo from "../../assets/img/cryptea1.PNG";
@@ -15,75 +16,168 @@ import DashHome from "./home";
 import { NavLink, 
         useLocation 
       } from "react-router-dom";
-
+import { useState } from 'react';
 
 const Dash = () => {
 
   const { pathname } = useLocation();
 
+  const [isOpen, close] = useState(false);
+
+  const [isOpen3, close3] = useState(false);
+
+  const toggle = () => {
+
+    close(!isOpen);
+
+    setTimeout(() => {
+        close3(!isOpen3);
+    }, 900)
+
+  }
+
   const active = "border-l-[3px] border-l-[#F57059] text-[#F57059]";
   return (
     <div className="h-full dash w-full bg-[#F9FAFF] flex">
-      <div className="sidebar min-w-[250px] bg-white border-solid border-r-[1px] border-r-[#E3E3E3] h-[inherit]">
-        <div className="w-full py-[1.6rem] border-solid border-b-[1px] flex justify-center items-center">
+      <div
+        className={`sidebar transition-all delay-500 ${
+          isOpen ? "min-w-[250px] w-[250px]" : "w-[50px] min-w-[50px]"
+        } bg-white border-solid border-r-[1px] border-r-[#E3E3E3] h-[inherit]`}
+      >
+        <div
+          className={`w-full ${
+            isOpen ? "py-[1.6rem]" : "py-[1rem]"
+          } border-solid innerSide border-b-[1px] flex justify-center transition-all delay-500 items-center relative`}
+        >
           <NavLink to="/" className="flex items-center justify-center">
-            <img src={logo} alt="cryptea" width={22} className="mr-[5px]" />{" "}
-            <img src={logo1} alt="cryptea" />
+            <img
+              src={logo}
+              alt="cryptea"
+              width={isOpen ? 22 : 40}
+              className={`mr-[5px] min-w-[${
+                isOpen ? 22 : 40
+              }px] transition-all delay-500`}
+            />{" "}
+            <img
+              src={logo1}
+              className={isOpen3 ? "" : "transition-all delay-500 hidden"}
+              alt="cryptea"
+            />
           </NavLink>
+          {isOpen3 ? (
+            <IoIosArrowDropleftCircle
+              onClick={toggle}
+              className={`transition-all cursor-pointer delay-500 text-[#F57059] absolute right-[-21px] m-auto z-50 top-0 bottom-0`}
+              size={30}
+            />
+          ) : (
+            <IoIosArrowDroprightCircle
+              onClick={toggle}
+              className={`transition-all cursor-pointer delay-500 text-[#F57059] absolute right-[-21px] m-auto z-50 top-0 bottom-0`}
+              size={30}
+            />
+          )}
         </div>
 
-        <div className="flex flex-col py-3 px-[27px] justify-center">
-          <div className="pb-9 mb-5 border-b-[#E3E3E3] border-b-[1px]">
+        <div
+          className={`flex innerSide transition-all delay-500 flex-col py-3 ${
+            isOpen ? "px-[27px]" : "px-[1px]"
+          } justify-center`}
+        >
+          <div
+            className={`${
+              isOpen3 ? "pb-9" : "pb-4"
+            } mb-5 border-b-[#E3E3E3] border-b-[1px]`}
+          >
             <div
-              className={`rounded-[4px] hover:border-l-[#F57059] hover:text-[#F57059] hover:bg-[#F5F8FE]  border-solid ${
+              className={`rounded-[4px] overflow-hidden flex-nowrap transition-all delay-500 hover:border-l-[#F57059] hover:text-[#F57059] hover:bg-[#F5F8FE] border-solid ${
                 pathname === "/dashboard" ? active : ""
-              } transition-all delay-500 bg-[#F5F8FE] flex items-center py-[9px]`}
+              } transition-all delay-500 bg-[#F5F8FE] py-[9px]`}
             >
-              <BiHomeAlt size={16.5} className="text-inherit mx-[15px]" />
-
-              <NavLink to="/dashboard" className="text-inherit text-[14px]">
-                Dashboard
+              <NavLink
+                to="/dashboard"
+                className="text-inherit flex items-center text-[14px]"
+              >
+                <BiHomeAlt
+                  size={isOpen3 ? 16.5 : 21}
+                  className={`text-inherit transition-all delay-500 ${
+                    isOpen3 ? "min-w-[16.5px]" : "min-w-[21px]"
+                  } mx-[15px]`}
+                />{" "}
+                {isOpen3 ? "Dashboard" : ""}
               </NavLink>
             </div>
           </div>
 
           <div
-            className={`rounded-[4px] my-1 ${
+            className={`rounded-[4px] overflow-hidden flex-nowrap  my-1 ${
               pathname.indexOf("") !== -1 ? active : ""
-            } border-solid hover:border-l-[3px] border-l-transparent text-[#A9A9A9] hover:border-l-[#F57059] hover:text-[#F57059] hover:bg-[#F5F8FE] flex items-center transition-all delay-500 py-[9px]`}
+            } border-solid hover:border-l-[3px] border-l-transparent text-[#A9A9A9] hover:border-l-[#F57059] hover:text-[#F57059] hover:bg-[#F5F8FE]  transition-all delay-500 py-[9px]`}
           >
-            <BiPaperPlane size={16.5} className="text-inherit mx-[15px]" />{" "}
-            <NavLink to="/dashboard" className="text-inherit text-[14px]">
-              Your Pages
+            <NavLink
+              to="/dashboard"
+              className="text-inherit flex items-center text-[14px]"
+            >
+              <BiPaperPlane
+                size={isOpen3 ? 16.5 : 21}
+                className={`text-inherit transition-all delay-500 ${
+                  isOpen3 ? "min-w-[16.5px]" : "min-w-[21px]"
+                } mx-[15px]`}
+              />{" "}
+              {isOpen3 ? "Your Pages" : ""}
             </NavLink>
           </div>
 
           <div
-            className={`rounded-[4px] my-1 border-solid hover:border-l-[3px] hover:border-l-[#F57059] border-l-transparent text-[#A9A9A9] hover:text-[#F57059] hover:bg-[#F5F8FE] flex items-center transition-all delay-500 py-[9px] ${
+            className={`rounded-[4px] overflow-hidden flex-nowrap my-1 border-solid hover:border-l-[3px] hover:border-l-[#F57059] border-l-transparent text-[#A9A9A9] hover:text-[#F57059] hover:bg-[#F5F8FE] transition-all delay-500 py-[9px] ${
               pathname.indexOf("") !== -1 ? active : ""
             }`}
           >
-            <BiCreditCard size={16.5} className="text-inherit mx-[15px]" />{" "}
-            <NavLink to="/dashboard" className="text-inherit text-[14px]">
-              Payment Links
+            <NavLink
+              to="/dashboard"
+              className="text-inherit flex items-center text-[14px]"
+            >
+              <BiCreditCard
+                size={isOpen3 ? 16.5 : 21}
+                className={`text-inherit transition-all delay-500  ${
+                  isOpen3 ? "min-w-[16.5px]" : "min-w-[21px]"
+                } mx-[15px]`}
+              />{" "}
+              {isOpen3 ? "Payment Links" : ""}
             </NavLink>
           </div>
 
           <div
-            className={`rounded-[4px] my-1 border-solid hover:border-l-[3px] hover:border-l-[#F57059] hover:text-[#F57059] border-l-transparent text-[#A9A9A9] hover:bg-[#F5F8FE] flex items-center transition-all delay-500 py-[9px] ${
+            className={`rounded-[4px] overflow-hidden flex-nowrap my-1 border-solid hover:border-l-[3px] hover:border-l-[#F57059] hover:text-[#F57059] border-l-transparent text-[#A9A9A9] hover:bg-[#F5F8FE] transition-all delay-500 py-[9px] ${
               pathname.indexOf("") !== -1 ? active : ""
             }`}
           >
-            <RiSettingsLine size={16.5} className="text-inherit mx-[15px]" />{" "}
-            <NavLink to="/dashboard" className="text-inherit text-[14px]">
-              Settings
+            <NavLink
+              to="/dashboard"
+              className="text-inherit flex items-center text-[14px]"
+            >
+              <RiSettingsLine
+                size={isOpen3 ? 16.5 : 21}
+                className={`text-inherit transition-all delay-500  ${
+                  isOpen3 ? "min-w-[16.5px]" : "min-w-[21px]"
+                } mx-[15px]`}
+              />{" "}
+              {isOpen3 ? "Settings" : ""}
             </NavLink>
           </div>
 
-          <div className="rounded-[4px] my-1 border-solid hover:border-l-[3px] hover:border-l-[#F57059] border-l-transparent text-[#A9A9A9] hover:text-[#F57059] hover:bg-[#F5F8FE] flex items-center transition-all delay-500 py-[9px]">
-            <BiLogOut size={16.5} className="text-inherit mx-[15px]" />{" "}
-            <NavLink to="dashboard/logout" className="text-inherit text-[14px]">
-              Log Out
+          <div className="rounded-[4px] overflow-hidden flex-nowrap my-1 border-solid hover:border-l-[3px] hover:border-l-[#F57059] border-l-transparent text-[#A9A9A9] hover:text-[#F57059] hover:bg-[#F5F8FE] transition-all delay-500 py-[9px]">
+            <NavLink
+              to="dashboard/logout"
+              className="text-inherit flex items-center text-[14px]"
+            >
+              <BiLogOut
+                size={isOpen3 ? 16.5 : 21}
+                className={`text-inherit transition-all delay-500 ${
+                  isOpen3 ? "min-w-[16.5px]" : "min-w-[21px]"
+                } mx-[15px]`}
+              />{" "}
+              {isOpen3 ? "Log Out" : ""}
             </NavLink>
           </div>
         </div>
@@ -95,7 +189,7 @@ const Dash = () => {
             <span>Hope you are healthy and happy today..</span>
           </div>
           <div className="flex items-center">
-            <form
+            {/* <form
               className="relative min-w-[260px] mr-[10px] flex items-center"
               method="get"
               action=""
@@ -111,18 +205,15 @@ const Dash = () => {
                 type="search"
                 name="username"
               />
-            </form>
+            </form> */}
 
             <div className="h-full w-[20px] mx-2">
               <BiBell size={23} className="cursor-pointer" color="#000" />
             </div>
 
-          <Avatar
-          sx={{ bgcolor: 'red'}}
-          alt="Zarror"
-        >
-          Z
-        </Avatar>
+            <Avatar sx={{ bgcolor: "red" }} alt="Zarror">
+              Z
+            </Avatar>
           </div>
         </div>
 
