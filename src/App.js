86869adf9f765949e916a94ac47a6b595b85a86d
maxtendies <103554120+maxtendies@users.noreply.github.com>
2022-05-16 23:06:24 +1200

    Minor changes to dashboard and loader
    added an svg file to replace the png for the logo and changed the png text to actual text so it's not blurry at different resolutions. Minor changes also made to the sidebar menu in the dashboard

--- src/App.js
+++ src/App.js
@@ -1,34 +1,45 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import About from "./pages/about";
 import Home from "./pages/Home";
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import Signup from "./pages/auth/signup";
-import Dash from './pages/dashboard';
-import Loader from './components/loader';
+import Dash from "./pages/dashboard";
+import Loader from "./components/loader";
 import Notfound from "./pages/error/404";
 import LoginForm from "./pages/auth/login";
 import { useMoralis } from "react-moralis";
 const App = () => {
   const { isAuthenticated, isInitializing } = useMoralis();
   const [loading, uloading] = useState(true);
-    useEffect(() => {
-        if (isInitializing) {
-          uloading(false)
-        }
-    }, [isInitializing])
+  useEffect(() => {
+    if (isInitializing) {
+      uloading(false);
+    }
+  }, [isInitializing]);
 
   return (
-     <BrowserRouter>
-        <Routes>
-          <Route path="/" element={loading ? <Loader /> : <Home />} />
-          <Route path="/about" element={loading ? <Loader /> : <About />} />
-          <Route path="/signup" element={loading ? <Loader /> : <Signup />} />
-          <Route path="/login" element={loading ? <Loader /> : <LoginForm />} />
-          <Route path="/dashboard" element={loading ? <Loader /> : (isAuthenticated ? <Dash /> : <Navigate to="/"/>)}/>
-          <Route path="*" element={loading ? <Loader /> : <Notfound /> }/>
-        </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={loading ? <Loader /> : <Home />} />
+        <Route path="/about" element={loading ? <Loader /> : <About />} />
+        <Route path="/signup" element={loading ? <Loader /> : <Signup />} />
+        <Route path="/login" element={loading ? <Loader /> : <LoginForm />} />
+        <Route
+          path="/dashboard"
+          element={
+            loading ? (
+              <Loader />
+            ) : isAuthenticated ? (
+              <Dash />
+            ) : (
+              <Navigate to="/" />
+            )
+          }
+        />
+        <Route path="*" element={loading ? <Loader /> : <Notfound />} />
+      </Routes>
+    </BrowserRouter>
   );
-}
+};
 
 export default App;
