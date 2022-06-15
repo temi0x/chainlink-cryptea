4c47d6840c94c2e30ede1bf391d5d2d670a439af
Temiloluwa Adelowo <moboluwaji003@gmail.com>
2022-06-15 13:40:44 +0100

    [fix]: minor change to connect modal

--- src/components/Nav/index.jsx
+++ src/components/Nav/index.jsx
@@ -166,10 +166,10 @@ function Nav() {
                             Connect Wallet
                           </h3>
                           <button
-                            className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
+                            className="p-1 ml-auto bg-transparent border-0 text-black float-right text-4xl leading-none font-semibold outline-none focus:outline-none"
                             onClick={() => setShowModal(false)}
                           >
-                            <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                            <span className="bg-transparent text-black h-6 w-6 text-4xl block outline-none focus:outline-none">
                               ×
                             </span>
                           </button>
