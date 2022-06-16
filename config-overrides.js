7ba91ce27909095bb0c4da149bed28f7831f090a
Temiloluwa Adelowo <moboluwaji003@gmail.com>
2022-06-16 03:54:11 +0100

    [chore]: walletconnect web3uikit installation brings up console-browserify error

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,12 @@
 module.exports = function override(config, env) {
-    console.log('override')
-    let loaders = config.resolve
-    loaders.fallback = {
-        zlib: require.resolve("browserify-zlib"),
-        stream: require.resolve("stream-browserify"),
-        https: require.resolve("https-browserify"),
-        http: require.resolve("stream-http"),
-        crypto: require.resolve("crypto-browserify")
-    }
-
-    return config
-}
+  console.log("override");
+  let loaders = config.resolve;
+  loaders.fallback = {
+    zlib: require.resolve("browserify-zlib"),
+    stream: require.resolve("stream-browserify"),
+    https: require.resolve("https-browserify"),
+    http: require.resolve("stream-http"),
+    crypto: require.resolve("crypto-browserify"),
+  };
+  return config;
+};
